2ce664acc8370e4923ac5dce54d5faad5d370431
Kyle <kjfulle@gmail.com>
2023-07-14 13:43:41 -0400

    Figured out how to play and save audio.  Code is in temp.py as a starting point.

---
+++ temp.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+import simpleaudio as sa
+import wave
+
+SAMPLE_RATE = 44_100
+
+def temp_play_and_save(duration, volume, frequency):
+
+    """
+    This function generates, plays, and saves some audio without obvious issue, 
+    but should not actually be used; it is meant mostly just to pick
+    """
+
+
+
+
+def main():
+
+    """ 
+    This code is just meant to be a starting point to pick up from later.  
+    It generates some example audio (a sine wave), plays it, and saves it.
+    """
+
+    duration = 3
+    volume = 0.02
+    frequency = 330
+
+    audio = np.sin(np.linspace(0, duration, SAMPLE_RATE * duration) * 2 * np.pi * frequency) * volume
+
+    wav_data = (audio * 2**15).astype(np.int16)
+
+    play_obj = sa.play_buffer(wav_data, 1, 2, SAMPLE_RATE)
+    play_obj.wait_done()
+
+    with wave.open('untitled.wav', 'w') as f:
+
+        f.setnchannels(1)
+        f.setframerate(SAMPLE_RATE)
+        f.setsampwidth(2)
+        f.writeframes(wav_data)
+
+
+
+if __name__ == "__main__":
+    main()

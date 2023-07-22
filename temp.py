78aab8074e56810552eedf93bf9ac8e3c9c80a1b
Kyle <kjfulle@gmail.com>
2023-07-22 14:10:43 -0400

    some temporary functions for audio generation and saving/playing in temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,33 @@
 
 import numpy as np
+from math import sin, pi
 import simpleaudio as sa
 import wave
 
 SAMPLE_RATE = 44_100
+
+def make_audio(duration, sampler, /):
+    """ TEMPORARY """
+    return np.array(list(map(sampler, np.linspace(0, duration, SAMPLE_RATE * duration))));
+
+def audio_to_wav_data(audio):
+    """ TEMPORARY """
+    return (audio * 2**15).astype(np.int16)
+
+def write_audio(audio, name='untitled.wav', /):
+    """ TEMPORARY """
+    wav_data = audio_to_wav_data(audio)
+    with wave.open(name, 'w') as f:
+        f.setnchannels(1)
+        f.setframerate(SAMPLE_RATE)
+        f.setsampwidth(2)
+        f.writeframes(wav_data)
+
+def play_audio(audio):
+    """ TEMPORARY """
+    wav_data = audio_to_wav_data(audio)
+    play_obj = sa.play_buffer(wav_data, 1, 2, SAMPLE_RATE)
+    return play_obj
 
 def main():
 
@@ -16,20 +40,10 @@ def main():
     volume = 0.02
     frequency = 330
 
-    audio = np.sin(np.linspace(0, duration, SAMPLE_RATE * duration) * 2 * np.pi * frequency) * volume
+    audio = make_audio(duration, lambda t: sin(t * 2 * pi * frequency) * volume)
 
-    wav_data = (audio * 2**15).astype(np.int16)
-
-    play_obj = sa.play_buffer(wav_data, 1, 2, SAMPLE_RATE)
-    play_obj.wait_done()
-
-    with wave.open('untitled.wav', 'w') as f:
-
-        f.setnchannels(1)
-        f.setframerate(SAMPLE_RATE)
-        f.setsampwidth(2)
-        f.writeframes(wav_data)
-
+    play_audio(audio).wait_done()
+    write_audio(audio, 'test.wav')
 
 
 if __name__ == "__main__":

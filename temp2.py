2f90fd59486df158058bcf4f1e78ad8b2bb9c0e2
unknown <rhaney601@gmail.com>
2023-07-22 15:28:51 -0400

    funky new volume function

--- temp2.py
+++ temp2.py
@@ -1,41 +1,9 @@
-import numpy as np
-from math import sin, pi
-import simpleaudio as sa
-import wave
+from temp import *
 
+"def two_notes"
 
-
-
-SAMPLE_RATE = 44_100
-
-
-
-def make_audio(duration, sampler, /):
-    """ TEMPORARY """
-    return np.array(list(map(sampler, np.linspace(0, duration, SAMPLE_RATE * duration))));
-
-def audio_to_wav_data(audio):
-    """ TEMPORARY """
-    return (audio * 2**15).astype(np.int16)
-
-def write_audio(audio, name='untitled.wav', /):
-    """ TEMPORARY """
-    wav_data = audio_to_wav_data(audio)
-    with wave.open(name, 'w') as f:
-        f.setnchannels(1)
-        f.setframerate(SAMPLE_RATE)
-        f.setsampwidth(2)
-        f.writeframes(wav_data)
-
-def play_audio(audio):
-    """ TEMPORARY """
-    wav_data = audio_to_wav_data(audio)
-    play_obj = sa.play_buffer(wav_data, 1, 2, SAMPLE_RATE)
-    return play_obj
-
-
-
-
+"""simple function that takes a volume function and a frequency function and
+ returns the audio that results from the 2"""
 def foo(duration, volume, frequency):
 
     audio = make_audio(duration, lambda t: sin(t * 2 * pi * frequency(t)) * volume(t))
@@ -50,8 +18,8 @@ def main():
     """
 
     duration = 10
-    volume = lambda t: 1 * ((t) * .01)
-    frequency = lambda t: 440 - ((t * 44))
+    volume = lambda t: (sin(2 * pi * t * 100 ) + 1 )/2
+    frequency = lambda t: 440
     """
     volume = 0.02
     frequency = 330

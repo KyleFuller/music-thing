d53eb3d6dbcda65a26cfccd26fb1bd77a042ad0a
Kyle <kjfulle@gmail.com>
2023-09-09 19:18:05 -0400

    fixed parentheses and off-by-one error that I accidentally reintroduced by deleting the wrong duplicate file

--- pitches.py
+++ pitches.py
@@ -1,14 +1,14 @@
 def C(n: int) -> float:
-    return 261.63 * (2 ** n -3)
+    return 261.63 * 2 ** (n - 4)
 def D(n: int) -> float:
-    return 293.66 * (2 ** n - 3)
+    return 293.66 * 2 ** (n - 4)
 def E(n: int) -> float:
-    return 329.63 * (2 ** n - 3)
+    return 329.63 * 2 ** (n - 4)
 def F(n: int) -> float:
-    return 349.23 * (2 ** n - 3)
+    return 349.23 * 2 ** (n - 4)
 def G(n: int) -> float:
-    return 392 * (2 ** n - 3)
+    return 392 * 2 ** (n - 4)
 def A(n: int) -> float:
-    return 440 * (2 ** n - 3)
+    return 440 * 2 ** (n - 4)
 def B(n: int) -> float:
-    return 493.88 * (2 ** n - 3)
+    return 493.88 * 2 ** (n - 4)

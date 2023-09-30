7dfb615a5ef7adf1f9e19129c08445f49beb4d72
KyleFuller <56740521+KyleFuller@users.noreply.github.com>
2023-09-30 00:47:03 -0400

    added caching to get_degree_sin_sound and make_audio to speed up audification of songs with many notes but few distinct notes.

--- get_degree_sin_sound.py
+++ get_degree_sin_sound.py
@@ -1,9 +1,18 @@
 from sound_aspects_to_sound import make_sound as _make_sound
 
 import math as _math
-from typing import Callable as _Callable
-    
-def get_degree_sin_sound(degree: int, key: _Callable[[int], float], duration: float, volume: float, /) -> tuple[_Callable[[float], float], float, float]:
+from typing import Callable as _Fn
+from functools import cache as _cache
+
+@_cache
+def get_degree_sin_sound(
+        degree: int, 
+        key: _Fn[[int], float], 
+        duration: float, 
+        volume: float, 
+        /
+    ) -> tuple[(_Fn[[float], float]), float, float]:
+
     sin = _math.sin
     pi = _math.pi
 
@@ -12,10 +21,10 @@ def get_degree_sin_sound(degree: int, key: _Callable[[int], float], duration: fl
     stop = duration
     auditory_duration = stop - start
     fudged_frequency = round(nonfudged_frequency * auditory_duration) / auditory_duration
-    waving_func: _Callable[[float, float], float] = lambda _, x: sin(2 * pi * x)
-    frequency_func: _Callable[[float], float] = lambda _: fudged_frequency
-    volume_func: _Callable[[float], float] = lambda _: volume
-    return _make_sound(waving_func, frequency_func,volume_func, start,stop), start, stop
+    get_waveform: _Fn[[float], _Fn[[float], float]] = lambda t: lambda x: sin(2 * pi * x)
+    get_frequency: _Fn[[float], float] = lambda t: fudged_frequency
+    get_volume: _Fn[[float], float] = lambda t: volume
+    return _make_sound(get_waveform, get_frequency, get_volume), start, stop
 
 def s_0_m1_1_0(d: int) -> list[int]:
 
@@ -29,12 +38,12 @@ def main():
     
     from scales import ionian_scale, dorian_scale, harmonic_scale, aeolian_scale, phrygian_scale, major_pentatonic_scale, minor_pentatonic_scale # type: ignore
 
-    degrees = range(6)
-    key = minor_pentatonic_scale(C(4))
+    degrees = s_0_m1_1_0(6)
+    key = aeolian_scale(C(4))
     duration = 0.25
     volume = 0.2
 
-    from audio import make_audio, play_audio
+    from audio import make_audio, play_audio # type: ignore
     audio: list[float] = []
     for degree in degrees:
         sound, start, stop = get_degree_sin_sound(degree, key, duration, volume)

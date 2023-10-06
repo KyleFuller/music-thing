c0d235e3dc4299789ae34db4351f66919ce32111
KyleFuller <56740521+KyleFuller@users.noreply.github.com>
2023-10-06 16:44:58 -0400

    added benchmark to test_integrate.py

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -14,12 +14,12 @@ def _is_approx(x: float, y: float, tol: float):
 
 def _integral_value_test(
         f: _RVFn, 
-        integrate: _Fn[[_RVFn, float], _RVFn] ,
+        integrate: _Fn[[_RVFn], _RVFn],
         ts: list[float], 
         true_integral: _RVFn, 
         tol: float):
     
-    integral = integrate(f, _SAMPLE_RATE)
+    integral = integrate(f)
 
     computed_vals = [integral(t) for t in ts]
     target_vals =  [true_integral(t) for t in ts]
@@ -28,31 +28,34 @@ def _integral_value_test(
         assert _is_approx(computed_vals[i], target_vals[i], tol)
 
 def test():
+    ts = _linspace(-0.5, 0.5, 10 * _SAMPLE_RATE)
 
-    
-    ts = _linspace(-0.5 + 0.001, 0.5 - 0.001, 10 * 44100)
+    integrate: _Fn[[_RVFn], _RVFn] = lambda f: _integrate(f, 4096)
 
     f: _RVFn = lambda t: 10 * t
     integral: _RVFn = lambda t: 5 * t ** 2
-    _integral_value_test(f, _integrate, ts, integral, tol=1e-14)
+    _integral_value_test(f, integrate, ts, integral, tol=1e-12)
 
     f: _RVFn = lambda t: _exp(- (10 * t)**2)
     integral: _RVFn = lambda t: _sqrt(_pi) / 2 * _erf(10 * t) / 10
-    _integral_value_test(f, _integrate, ts, integral, tol=1e-15)
+    _integral_value_test(f, integrate, ts, integral, tol=1e-12)
 
     f: _RVFn = lambda t: _exp(5 * t)
     integral: _RVFn = lambda t: (_exp(5 * t) - 1) / 5
-    _integral_value_test(f, _integrate, ts, integral, tol=1e-13)
+    _integral_value_test(f, integrate, ts, integral, tol=1e-12)
 
     f: _RVFn = lambda t: _sin(5 * t)
     integral: _RVFn = lambda t: (1 -_cos(5 * t)) / 5
-    _integral_value_test(f, _integrate, ts, integral, tol=1e-14)
+    _integral_value_test(f, integrate, ts, integral, tol=1e-12)
 
     f: _RVFn = lambda t: t ** (1/3) if t >= 0 else -(-t)**(1/3)
     integral: _RVFn = lambda t: 3 / 4 * t * f(t)
-    _integral_value_test(f, _integrate, ts, integral, tol=1e-7)
+    _integral_value_test(f, lambda f: _integrate(f, _SAMPLE_RATE), ts, integral, tol=1e-7)
 
-
+def benchmark(nsec: int, step_rate:float, /):
+    ts = _linspace(-nsec/2, nsec/2, nsec * _SAMPLE_RATE)
+    integral = _integrate(lambda x: _sin(x), step_rate)
+    [integral(t) for t in ts]
 
 if __name__ == '__main__':
     test()

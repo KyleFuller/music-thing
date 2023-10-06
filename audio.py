5a47884f96dde507c52e159aa693128ba1b13b9a
KyleFuller <56740521+KyleFuller@users.noreply.github.com>
2023-10-06 17:42:23 -0400

    loosened hinted parameter types from list to Iterable where possible.

--- audio.py
+++ audio.py
@@ -4,18 +4,18 @@ import math as _math
 import simpleaudio as _sa # type: ignore
 import wave as _wave
 import struct as _struct
-from typing import Callable as _Fn
+from typing import Callable as _Fn, Iterable as _Iterable
 
 SAMPLE_RATE = 44_100
 
 def make_audio(sound: _Fn[[float], float], start: float, stop: float, /) -> list[float]:
     return [sound(s) for s in _linspace(start, stop, _math.ceil(SAMPLE_RATE * (stop - start)))]
 
-def audio_to_wav_data(audio: list[float], /) -> bytes:
+def audio_to_wav_data(audio: _Iterable[float], /) -> bytes:
     int_vals = [int(s * (2**15 - 1)) for s in audio]
     return _struct.pack(f"<{len(int_vals)}h", *int_vals)
 
-def write_audio(audio: list[float], name:str='untitled.wav', /):
+def write_audio(audio: _Iterable[float], name:str='untitled.wav', /):
     with _wave.open(name, 'w') as f:
         f.setnchannels(1)
         f.setframerate(SAMPLE_RATE)
@@ -23,7 +23,7 @@ def write_audio(audio: list[float], name:str='untitled.wav', /):
         
         f.writeframes(audio_to_wav_data(audio))
 
-def play_audio(audio: list[float], /):
+def play_audio(audio: _Iterable[float], /):
     wav_data = audio_to_wav_data(audio)
     play_obj = _sa.play_buffer(wav_data, 1, 2, SAMPLE_RATE) # type: ignore
     return play_obj

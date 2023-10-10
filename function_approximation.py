ccaf8f0e57c00cad69b1a497fda44971d7535d3a
KyleFuller <56740521+KyleFuller@users.noreply.github.com>
2023-10-10 17:41:25 -0400

    documented basic approximation functions

--- function_approximation.py
+++ function_approximation.py
@@ -2,13 +2,22 @@ from typing import Callable as _Fn
 import math as _math
 
 def linear_interpolate(left: float, right: float, /) -> _Fn[[float], float]:
-
+    """
+    Takes the value on the left and right and returns a linear function that interpolates between them at a given 
+    fraction mu along from the left point toward the right point.  mu < 0 or mu > 1 gives extrapolation.
+    """
     def interpolation(mu:float):
         return left * (1 - mu) + right * mu
     
     return interpolation
 
 def quadratic_interpolate(left: float, right: float, righter: float, /) -> _Fn[[float], float]:
+    """
+    Works similarly to quadratic approximate, but the returned function is guaranteed to interpolate through the input
+    values, at the cost of worse overall convergence.
+
+    Produces asymptotically faster convergence as linear_interpolate.
+    """
     def interpolation(mu: float):
 
         a = 0.5 * (left - 2 * right + righter)
@@ -20,6 +29,14 @@ def quadratic_interpolate(left: float, right: float, righter: float, /) -> _Fn[[
     return interpolation
 
 def quadratic_approximate(lefter: float, left: float, right: float, righter: float, /) -> _Fn[[float], float]:
+    """
+    Takes a value on the left, a value on the right, and a value one more step to the right from there, and returns a 
+    quadratic approximation function where the input mu determines a potentially fractional or negative number of steps 
+    from the left at which to approximate.
+
+    Produces asymptotically faster convergence than quadratic_interpolate, and has the same rate of convergence as 
+    catmull_rom_interpolate, although it does not interpolate through all four points provided.
+    """
     q_left = quadratic_interpolate(lefter, left, right)
     q_right = quadratic_interpolate(righter, right, left)
     def approximation(mu: float):
@@ -28,6 +45,13 @@ def quadratic_approximate(lefter: float, left: float, right: float, righter: flo
 
 
 def catmull_rom_interpolate(lefter: float, left: float, right: float, righter: float, /) -> _Fn[[float], float]:
+    """
+    Takes four values at equally spaced steps and returns a function that interpolates through all of them, where the 
+    input to the interpolant is the possibly fractional or negative number of steps from the left at which to interpolate.
+
+    Produces asymptotically faster convergence than quadratic_interpolate, and the same rate of 
+    convergence as quadratic_approximate.
+    """
     def interpolation(mu: float):
         mu_sq = mu * mu
         a0 = -0.5*lefter + 1.5*left - 1.5*right + 0.5*righter
@@ -38,6 +62,15 @@ def catmull_rom_interpolate(lefter: float, left: float, right: float, righter: f
     return interpolation
 
 def cubic_approximate(lefter: float, left: float, right: float, righter: float, /) -> _Fn[[float], float]:
+    """
+    Takes four values at equally spaced steps and returns a function that approximate through all of them, where the 
+    input to the approximation function is the possibly fractional or negative number of steps from the left at which 
+    to approximate.  This function might actually interpolate between the four points given, but we haven't yet bothered
+    to prove this to be the case.  
+
+    This method of approximation seems to produce asymptotically faster convergence than catmull_rom_interpolate or
+    quadratic_approximate.
+    """
     cr = catmull_rom_interpolate(lefter, left, right, righter)
     q = quadratic_approximate(lefter, left, right, righter)
     

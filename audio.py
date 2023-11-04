a75b9bd797469019fbea76f3abbd699197c2356a
KyleFuller <56740521+KyleFuller@users.noreply.github.com>
2023-11-03 20:46:29 -0400

    made play_audio demand-driven so that it only requests more samples as it's about to play them, rather than first going through the entire iterable and converting it to audio.  for long audio iterables, this drastically hastens the start of the audio playing while saving memory.  this also means playing infinitely long audio is now supported.
    the file name in write_audio is now required, and audio_to_wav_data has been renamed to audio_to_bytes.  make_audio now returns a lazy iterable of floats rather than a list.
    
    all functions in this file have now been documented.

--- audio.py
+++ audio.py
@@ -1,29 +1,43 @@
-from utils import linspace as _linspace
-
 import math as _math
-import simpleaudio as _sa # type: ignore
 import wave as _wave
 import struct as _struct
 from typing import Callable as _Fn, Iterable as _Iterable
+import sounddevice as _sd # type: ignore
+
+from utils import linspace as _linspace
 
 SAMPLE_RATE = 44_100
 
-def make_audio(sound: _Fn[[float], float], start: float, stop: float, /) -> list[float]:
-    return [sound(s) for s in _linspace(start, stop, _math.ceil(SAMPLE_RATE * (stop - start)))]
+def make_audio(sound: _Fn[[float], float], interval: tuple[float, float], /) -> _Iterable[float]:
+    start, stop = interval
+    """ returns an iterable of audio samples from evaluating the sound function
+    across the given interval, which has format (<start>, <stop>). """
+    return (sound(s) for s in _linspace(start, stop, _math.ceil(SAMPLE_RATE * (stop - start))))
 
-def audio_to_wav_data(audio: _Iterable[float], /) -> bytes:
+def audio_to_bytes(audio: _Iterable[float], /) -> bytes:
+    """ converts an iterable of audio samples to a bytes object of a format that
+    can be used in audio output. """
     int_vals = [int(s * (2**15 - 1)) for s in audio]
     return _struct.pack(f"<{len(int_vals)}h", *int_vals)
 
-def write_audio(audio: _Iterable[float], name:str='untitled.wav', /):
+def write_audio(audio: _Iterable[float], name:str, /):
+    """ writes the given iterable of audio samples to a single-channel wav file 
+    of the given name and in the appropriate format, replacing the original file
+    if already present. """
     with _wave.open(name, 'w') as f:
         f.setnchannels(1)
         f.setframerate(SAMPLE_RATE)
         f.setsampwidth(2)
         
-        f.writeframes(audio_to_wav_data(audio))
+        f.writeframes(audio_to_bytes(audio))
 
 def play_audio(audio: _Iterable[float], /):
-    wav_data = audio_to_wav_data(audio)
-    play_obj = _sa.play_buffer(wav_data, 1, 2, SAMPLE_RATE) # type: ignore
-    return play_obj
+    """ plays a given iterable of single-channel audio samples in a more-or-less
+    demand-driven fashion. thus, very long or infinitely long audio iterables are 
+    supported."""
+    stream = _sd.RawOutputStream(SAMPLE_RATE, channels=1, dtype='int16')
+    stream.start()
+    for s in audio:
+        the_bytes = audio_to_bytes([s])
+        stream.write(the_bytes) # type: ignore
+    stream.stop()
